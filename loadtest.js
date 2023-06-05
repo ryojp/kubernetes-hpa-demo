04420aee585e8521fb617949d7fb6a53be5f7204
Ryo Koike <ryok022366@gmail.com>
2023-06-05 21:51:51 +0900

    optimize HPA params and load-test for part 2

--- loadtest.js
+++ loadtest.js
@@ -3,12 +3,12 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
+        { duration: '1m', target: 30 },
         { duration: '1m', target: 100 },
-        { duration: '1m', target: 200 },
-        { duration: '3m', target: 800 },
-        { duration: '1m', target: 900 },
-        { duration: '2m', target: 500 },
-        { duration: '2m', target: 200 },
+        { duration: '3m', target: 200 },
+        { duration: '1m', target: 300 },
+        { duration: '2m', target: 100 },
+        { duration: '2m', target: 20 },
     ]
 }
 
